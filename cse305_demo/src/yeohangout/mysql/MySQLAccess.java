512d5c64318c583a20eb4ce7eae1a231fd043b9f
Jude <kw021992@gmail.com>
2017-12-03 22:42:41 -0500

    "REVERT"

--- cse305_demo/src/yeohangout/mysql/MySQLAccess.java
+++ cse305_demo/src/yeohangout/mysql/MySQLAccess.java
@@ -15,10 +15,9 @@ import java.sql.Statement;
  * http://www.vogella.com/tutorials/MySQLJava/article.html
  */
 public class MySQLAccess {
-	private Connection connect = null;
-    private Statement statement = null;
-    private PreparedStatement preparedStatement = null;
-    private ResultSet resultSet = null;
+	private Connection connect;
+    private Statement statement;
+    private ResultSet resultSet;
 
 	public void readDataBase() throws Exception {
         try {
@@ -29,69 +28,15 @@ public class MySQLAccess {
                     .getConnection("jdbc:mysql://mysql2.cs.stonybrook.edu:3306/howoo?"
                             + "user=howoo&password=111255764");
 
-//            // Statements allow to issue SQL queries to the database
-//            statement = connect.createStatement();
-//            // Result set get the result of the SQL query
-//            resultSet = statement
-//            			.executeQuery("select * from howoo.customer");
-//            writeResultSet(resultSet);
-//
-//
-//            preparedStatement = connect
-//                    .prepareStatement("SELECT id, Name from howoo.airline");
-//            resultSet = preparedStatement.executeQuery();
-//            writeResultSet(resultSet);
-//
-//            // Remove again the insert comment
-//            preparedStatement = connect
-//            .prepareStatement("delete from howoo.airline where id= ? ; ");
-//            preparedStatement.setString(1, "KA");
-//            preparedStatement.executeUpdate();
-//
-//            resultSet = statement
-//            .executeQuery("select * from howoo.airline");
-//            writeMetaData(resultSet);
-
         } catch (Exception e) {
             throw e;
-        } finally {
-            //close();
-        }
-
+        } 
     }
 	
 	public Connection getConnection() {
 		return connect;
 	}
 	
-
-    private void writeMetaData (ResultSet resultSet) throws SQLException {
-        //  Now get some metadata from the database
-        // Result set get the result of the SQL query
-
-        System.out.println("The columns in the table are: ");
-
-        System.out.println("Table: " + resultSet.getMetaData().getTableName(1));
-        for  (int i = 1; i<= resultSet.getMetaData().getColumnCount(); i++){
-            System.out.println("Column " +i  + " "+ resultSet.getMetaData().getColumnName(i));
-        }
-    }
-
-    private void writeResultSet(ResultSet resultSet) throws SQLException {
-        // ResultSet is initially before the first data set
-        while (resultSet.next()) {
-            // It is possible to get the columns via name
-            // also possible to get the columns via the column number
-            // which starts at 1
-            // e.g. resultSet.getSTring(2);
-            String id = resultSet.getString("id");
-            String Name = resultSet.getString("Name");
-            System.out.println("id: " + id);
-            System.out.println("Name: " + Name);
-        }
-    }
-
-    // You need to close the resultSet
     public void close() {
         try {
             if (resultSet != null) {
--- cse305_demo/src/yeohangout/servlet/dashboard/GetRevenueServlet.java
+++ cse305_demo/src/yeohangout/servlet/dashboard/GetRevenueServlet.java
@@ -1,11 +1,22 @@
 package yeohangout.servlet.dashboard;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import yeohangout.javabeans.Revenue;
 
 /**
  * Servlet implementation class GetRevenue
@@ -13,21 +24,160 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(name = "view-revenue", urlPatterns = { "/view-revenue" })
 public class GetRevenueServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public GetRevenueServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	private PreparedStatement ps;
+	private ResultSet rs;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public GetRevenueServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		// This will load the MySQL driver, each DB has its own driver
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			// Setup the connection with the DB
+			Connection connect = DriverManager
+					.getConnection("jdbc:mysql://mysql2.cs.stonybrook.edu:3306/howoo?"
+							+ "user=howoo&password=111255764");
+			try {
+				// Execute SQL query
+				ps = connect.prepareStatement("SELECT * FROM RevnCity");
+				rs = ps.executeQuery();
+
+				ArrayList<Revenue> revnCityArr = new ArrayList<Revenue>();
+
+				if (rs != null) {
+					while (rs.next()) {
+						revnCityArr.add(new Revenue(rs.getInt("ResrNo"), rs.getTimestamp("ResrDate"), rs.getDouble("BookingFee"), 
+								rs.getDouble("TotalFare"), rs.getInt("RepSSN"), rs.getInt("AccountNo")));
+					}
+					HttpSession session = request.getSession();
+					session.setAttribute("revnCityArr", revnCityArr);
+				}
+				else {
+					RequestDispatcher rd = getServletContext().getRequestDispatcher("/dashboard-manager/dashboard-manager-employee.jsp");
+					System.out.println("No user found with given input, please register first.");
+					rd.include(request, response);
+				}
+
+			} finally {
+				try {
+					rs.close();
+					ps.close();
+				} catch (SQLException e) {
+					System.out.println("No user found with given input, please register first.");
+				}
+			}
+
+			try {
+				// Execute SQL query
+				ps = connect.prepareStatement("SELECT * FROM RevnCust");
+				rs = ps.executeQuery();
+
+				ArrayList<Revenue> revnCustArr = new ArrayList<Revenue>();
+
+				if (rs != null) {
+					while (rs.next()) {
+						resrArrFlight.add(new Revenue(rs.getString("AirlineID"), rs.getInt("FlightNo"), 
+								rs.getInt("ResrNo"), rs.getTimestamp("ResrDate"), rs.getDouble("BookingFee"), 
+								rs.getDouble("TotalFare"), rs.getInt("RepSSN"), rs.getInt("AccountNo")));
+					}
+					HttpSession session = request.getSession();
+					session.setAttribute("revnCustArr", revnCustArr);
+				}
+				else {
+					RequestDispatcher rd = getServletContext().getRequestDispatcher("/dashboard-manager/dashboard-manager-employee.jsp");
+					System.out.println("No user found with given input, please register first.");
+					rd.include(request, response);
+				}
+
+			} finally {
+				try {
+					rs.close();
+					ps.close();
+				} catch (SQLException e) {
+					System.out.println("No user found with given input, please register first.");
+				}
+			}
+
+			try {
+				// Execute SQL query
+				ps = connect.prepareStatement("SELECT * FROM RevnCustRep");
+				rs = ps.executeQuery();
+
+				ArrayList<Revenue> revnCustRepArr = new ArrayList<Revenue>();
+
+				if (rs != null) {
+					while (rs.next()) {
+						revnCustRepArr.add(new Revenue(rs.getString("Name"), rs.getInt("AccountNo"), rs.getInt("ResrNo")));
+					}
+					HttpSession session = request.getSession();
+					session.setAttribute("revnCustRepArr", revnCustRepArr);
+					String contextPath = request.getContextPath();
+					response.sendRedirect(contextPath + "/dashboard-manager/dashboard-manager-reservation.jsp");
+				}
+				else {
+					RequestDispatcher rd = getServletContext().getRequestDispatcher("/dashboard-manager/dashboard-manager-employee.jsp");
+					System.out.println("No user found with given input, please register first.");
+					rd.include(request, response);
+				}
+
+				connect.close();
+
+			} finally {
+				try {
+					rs.close();
+					ps.close();
+				} catch (SQLException e) {
+					System.out.println("No user found with given input, please register first.");
+				}
+			}
+			
+			try {
+				// Execute SQL query
+				ps = connect.prepareStatement("SELECT * FROM RevnFlight");
+				rs = ps.executeQuery();
+
+				ArrayList<Revenue> resrArrCust = new ArrayList<Revenue>();
+
+				if (rs != null) {
+					while (rs.next()) {
+						resrArrCust.add(new Reservation(rs.getString("Name"), rs.getInt("AccountNo"), rs.getInt("ResrNo")));
+					}
+					HttpSession session = request.getSession();
+					session.setAttribute("resrArrCust", resrArrCust);
+					String contextPath = request.getContextPath();
+					response.sendRedirect(contextPath + "/dashboard-manager/dashboard-manager-reservation.jsp");
+				}
+				else {
+					RequestDispatcher rd = getServletContext().getRequestDispatcher("/dashboard-manager/dashboard-manager-employee.jsp");
+					System.out.println("No user found with given input, please register first.");
+					rd.include(request, response);
+				}
+
+				connect.close();
+
+			} finally {
+				try {
+					rs.close();
+					ps.close();
+				} catch (SQLException e) {
+					System.out.println("No user found with given input, please register first.");
+				}
+			}
+
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
 	}
 
 	/**
@@ -37,5 +187,4 @@ public class GetRevenueServlet extends HttpServlet {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
-
 }
